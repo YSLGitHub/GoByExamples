220c3a4ebe38062daceb82014310a1314024c30b
YSLGitHub <33714994+YSLGitHub@users.noreply.github.com>
2020-12-23 21:53:13 +0800

    Create 11-range.go

---
+++ 11-range.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    nums := []int{2, 3, 4}
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    fmt.Println("sum:", sum)
+
+    for i, num := range nums {
+        if num == 3 {
+            fmt.Println("index:", i)
+        }
+    }
+
+    kvs := map[string]string{"a": "apple", "b": "banana"}
+    for k, v := range kvs {
+        fmt.Printf("%s -> %s\n", k, v)
+    }
+
+    for k := range kvs {						//range 也可以只遍历 map 的键
+        fmt.Println("key:", k)
+    }
+
+    for i, c := range "go" {					//range 在字符串中迭代 unicode 码点(code point)。 第一个返回值是字符的起始字节位置，然后第二个是字符本身。
+        fmt.Println(i, c)
+    }
+}
